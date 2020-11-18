2f6c90952a6736ea93944e4c18e4570b26b2261c
Alberto <albertoue93@gmail.com>
2020-11-17 19:12:53 -0600

    Aparto Model - Controller commit

--- app/Aparto.php
+++ app/Aparto.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Aparto extends Model
 {
     //
+    protected $table = "apartos";
+
+    protected $fillable = [
+        'numeroAparto','mts2','finca_id' 
+    ];
 }
